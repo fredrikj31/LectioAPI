3ae701f2b03c95e287510e58ca75237642bf645f
fredrikj31 <fredrik1206@gmail.com>
2020-05-07 18:27:03 +0200

    Added dyno specs

--- resources/schedule.py
+++ resources/schedule.py
@@ -6,7 +6,11 @@ import markdown
 class Schedule(Resource):
 	def get(self):
 		parser = reqparse.RequestParser()
-		parser.add_argument('testKey', location='headers')
+		parser.add_argument('testKey', location='header')
 		args = parser.parse_args()
 
-		return {'message': 'Success', 'data': "Here is where the data is going" + args['testKey']}, 200
+
+		if args['testKey'] is None:
+			return {'message': 'Error', 'data': "Unauthorized. Please enter login details"}, 401
+		else:
+			return {'message': 'Success', 'data': "Here is where the data is going" + args['testKey']}, 200
